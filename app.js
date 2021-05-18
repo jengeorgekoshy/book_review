215e38e82cfbd4824157e346e44b17c01057adc8
Jen George Koshy <jengeorgekoshy333@gmail.com>
2021-05-18 23:33:23 +0530

    connected with the mongodb and also added the login and register

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const app = express();
+const mongoose = require('mongoose')
+
+const { MONGOURL } = require('./keys')
+
+
+mongoose.connect(MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on("connected", () => {
+    console.log('mongo connected')
+})
+mongoose.connection.on("error", () => {
+    console.log('error')
+})
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+})
+
+
+const User = new mongoose.model("User", userSchema)
 
 
 app.use(express.static("public"))
@@ -21,6 +45,42 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
     res.render("register")
+})
+
+app.get("/logout", (req, res) => {
+    res.render("home")
+})
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.render('secrets')
+        }
+    })
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, (err, foundUser) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets")
+                }
+            }
+        }
+    })
 })
 
 app.listen(3000, () => {
